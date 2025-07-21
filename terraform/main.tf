baf225f7bc42f5ba8bd87dc587e63cbb602fcdd2
Praveen Jha <praveenjhafbg54321@gmail.com>
2025-07-21 13:41:26 +0530

    new account added

---
+++ terraform/main.tf
@@ -0,0 +1,35 @@
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "utkarsh.219302026@muj.manipal.edu"
+    AccountName               = "sandbox-aft"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "praveen.jha@celebaltech.com"
+    SSOUserFirstName          = "praveen"
+    SSOUserLastName           = "jha"
+  }
+
+  account_tags = {
+    "ABC:Owner"       = "dhruv.dalsania@celebaltech.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
